c2f595b3c8f2f99abe09a15ae16c2ea58a6e56ca
Scott R. Santos <santos@auburn.edu>
2017-12-14 23:01:12 -0600

    Added video playback for spawning mpv

--- luakit/.config/luakit/rc.lua
+++ luakit/.config/luakit/rc.lua
@@ -166,6 +166,17 @@ local view_source = require "view_source"
 -- Load user preferences
 local userprefs = require "userprefs"
 
+-- Play Embedded Video in External Player.
+ modes.add_binds("normal", {  
+       { "v", "Play video in page", function (w)  
+        local view = w.view  
+         local uri = view.hovered_uri or view.uri
+          if uri then  
+           luakit.spawn(string.format("mpv --geometry=640x360 %s", uri ))  
+        end  
+    end },  
+})
+
 -----------------------------
 -- End user script loading --
 -----------------------------
